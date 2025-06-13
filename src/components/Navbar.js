67ab86759bfe7f5468faf72c38ebe9e447fec355
Kenlove Chereste <cherestek@gmail.com>
2025-06-13 19:25:29 -0400

    Correction for commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,10 +26,12 @@ const Navbar = () => {
     <>
       <header className="navbar fade-in-hidden" ref={navRef}>
         <div className="navbar-container">
+          {/* Logo NON CLIQUABLE, aligné à gauche */}
           <div className="navbar-logo">
-            <img src={logo} alt="Chavidiomas Logo" />
+            <img src={logo} alt="Logo" />
           </div>
 
+          {/* Liens de navigation */}
           <nav className="navbar-links">
             <Link to="/">Início</Link>
             <Link to="/servicos">Serviços</Link>
